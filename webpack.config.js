32947dc5b0e5e90d41e5f9eeffc726948467be14
Tucker Whitehouse <TuckerWhitehouse@gmail.com>
2016-07-24 17:16:15 -0400

    Webpack Updates

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,9 @@ module.exports = {
     path: 'dist',
     filename: '[name].js'
   },
+  // resolve: {
+  //   modulesDirectories: ['node_modules', '.']
+  // },
   module: {
     loaders: [{
       test: /\.js$/,
